db097e4346bb65cc39262c33d399310426fb6f6d
SuthanK10 <suthankuruvayooran10@gmail.com>
2026-01-30 17:35:36 +0530

    feat: Add initial Cloudinary configuration file.

---
+++ config/cloudinary.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cloudinary Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your Cloudinary settings.
+    |
+    */
+
+    'notification_url' => env('CLOUDINARY_NOTIFICATION_URL'),
+
+    'cloud_url' => env('CLOUDINARY_URL'),
+
+    'upload_preset' => env('CLOUDINARY_UPLOAD_PRESET'),
+
+    'upload_route' => env('CLOUDINARY_UPLOAD_ROUTE'),
+
+    'upload_action' => env('CLOUDINARY_UPLOAD_ACTION'),
+
+];
