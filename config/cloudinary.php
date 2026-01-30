3cf0d6195189abe90eb95b654c67597d170f8cf0
SuthanK10 <suthankuruvayooran10@gmail.com>
2026-01-30 18:07:15 +0530

    feat: add Cloudinary configuration file

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -13,7 +13,7 @@ return [
 
     'notification_url' => env('CLOUDINARY_NOTIFICATION_URL'),
 
-    // 'cloud_url' => env('CLOUDINARY_URL', 'cloudinary://0:0@0'),
+    'cloud_url' => env('CLOUDINARY_URL', 'cloudinary://0:0@0'),
 
     'upload_preset' => env('CLOUDINARY_UPLOAD_PRESET'),
 
